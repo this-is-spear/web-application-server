5b3c9a6fd323d2ccb86dcb3324257756da4721a1
tis <92219795+this-is-spear@users.noreply.github.com>
2022-07-21 01:08:39 +0900

    Feat : /index.html URL 추가

--- src/main/java/util/IOUtils.java
+++ src/main/java/util/IOUtils.java
@@ -1,9 +1,18 @@
 package util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.RequestHandler;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class IOUtils {
+    private static final String NEW_LINE = System.getProperty("line.separator");
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+
     /**
      * @param BufferedReader는
      *            Request Body를 시작하는 시점이어야
@@ -16,5 +25,22 @@ public class IOUtils {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
         return String.copyValueOf(body);
+    }
+
+    public static String getRequestMessage(InputStream in) throws IOException {
+        log.info("Get Request Message");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        StringBuilder result = new StringBuilder();
+        for (String line; (line = reader.readLine()) != null; ) {
+            if (line.equals("")) {
+                break;
+            }
+            if (result.length() > 0) {
+                result.append(NEW_LINE);
+            }
+            result.append(line);
+        }
+        log.info("Get Request message, Message is \n {}", result);
+        return result.toString();
     }
 }
--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,17 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.IOUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Objects;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String NEW_LINE = System.getProperty("line.separator");
+    public static final String REGEX = " ";
 
     private Socket connection;
 
@@ -20,17 +21,26 @@ public class RequestHandler extends Thread {
 
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+            connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String requestMessage = IOUtils.getRequestMessage(in);
+            String url = getUrl(requestMessage);
+            if ("/index.html".equals(url)) {
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] body = "Hello World".getBytes();
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private String getUrl(String requestMessage) {
+        String url = requestMessage.split(NEW_LINE)[0].split(REGEX)[1];
+        log.info("Get URI {}", url);
+        return url;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
